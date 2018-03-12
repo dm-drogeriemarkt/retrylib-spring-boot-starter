41d326fbee538a0f425855ab8bfc688a7cd5f527
Markus Kiss <markus.kiss@dm.de>
2018-03-12 20:12:27 +0100

    Added Spring Boot autoconfiguration and an empty properties class for retrylib. More to follow.

--- src/main/java/de/dm/retrylib/RetrylibAutoConfiguration.java
+++ src/main/java/de/dm/retrylib/RetrylibAutoConfiguration.java
@@ -1,16 +1,47 @@
 package de.dm.retrylib;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.openhft.chronicle.map.ChronicleMap;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
-public class ChronicleMapConfig {
+@EnableScheduling
+public class RetrylibAutoConfiguration {
+
+    @ConditionalOnMissingBean(RetryService.class)
+    @Bean
+    public RetryService retryService(ObjectMapper objectMapper, @Value("#{retryMap}") ChronicleMap<String, RetryEntity> retryMap) {
+        return new RetryService(objectMapper, retryMap);
+    }
+
+    @ConditionalOnMissingBean(RetryProcessor.class)
+    @Bean
+    public RetryProcessor retryProcessor(RetryService retryService, List<RetryHandler> retryHandlers, ObjectMapper objectMapper) {
+        return new RetryProcessor(retryService, retryHandlers, objectMapper);
+    }
+
+    @ConditionalOnMissingBean(RetryHandler.class)
+    @Bean
+    public RetryHandler noOpRetryHandler() {
+        return new RetryHandler() {
+            @Override
+            public void handleWithRetry(Object payload) {}
+
+            @Override
+            public String retryType() {
+                return "";
+            }
+        };
+    }
 
     @Bean
     public ChronicleMap<String, RetryEntity> retryMap(@Value("${retrylib.persistence.maxEntries:1000000}") Long maxEntries,
@@ -36,4 +67,5 @@ public class ChronicleMapConfig {
                 .averageValueSize(averageValueSize)
                 .createPersistedTo(new File(fileLocation));
     }
+
 }
---
+++ src/main/java/de/dm/retrylib/RetrylibProperties.java
@@ -0,0 +1,11 @@
+package de.dm.retrylib;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "retrylib")
+public class RetrylibProperties {
+
+
+
+
+}
