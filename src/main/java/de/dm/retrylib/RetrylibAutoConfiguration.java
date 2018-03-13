78864e54d3f968a5f604c1e28a18dcfde86b2885
Markus Kiss <markus.kiss@dm.de>
2018-03-13 10:56:50 +0100

    Added additional recovery code on startup if the ChronicleMap gets corrupted.

--- src/main/java/de/dm/retrylib/RetrylibAutoConfiguration.java
+++ src/main/java/de/dm/retrylib/RetrylibAutoConfiguration.java
@@ -2,6 +2,9 @@ package de.dm.retrylib;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +14,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableScheduling
 public class RetrylibAutoConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RetrylibAutoConfiguration.class);
+
 
     @ConditionalOnMissingBean(RetryService.class)
     @Bean
@@ -48,7 +56,6 @@ public class RetrylibAutoConfiguration {
                                                       @Value("${retrylib.persistence.averageValueSize:600}") Double averageValueSize,
                                                       @Value("${retrylib.persistence.filePath}") String filePath,
                                                       @Value("${retrylib.persistence.fileName}") String fileName) throws IOException {
-
         if (filePath.isEmpty()) {
             filePath = System.getProperty("java.io.tmpdir");
         }
@@ -59,13 +66,52 @@ public class RetrylibAutoConfiguration {
 
         String fileLocation = String.format("%s/%s", filePath, fileName);
 
+        ChronicleMap<String, RetryEntity> retryMapWithoutRecovery = null;
+
+        try {
+            retryMapWithoutRecovery = createChronicleMapBuilder(maxEntries, averageValueSize).createPersistedTo(new File(fileLocation));
+            checkIfMapHasOnlyValidEntries(retryMapWithoutRecovery);
+            return retryMapWithoutRecovery;
+        } catch (IOException caughtException) {
+            return recoverMapFromInvalidHeader(maxEntries, averageValueSize, fileLocation, caughtException);
+        } catch (RuntimeException caughtException) {
+            if (retryMapWithoutRecovery != null) {
+                retryMapWithoutRecovery.close();
+            }
+            return recoverMapFromInvalidEntries(maxEntries, averageValueSize, fileLocation, caughtException);
+        }
+    }
+
+    private ChronicleMapBuilder<String, RetryEntity> createChronicleMapBuilder(Long entries, Double averageValueSize) {
         return ChronicleMap
                 .of(String.class, RetryEntity.class)
                 .name("retryMap")
-                .entries(maxEntries)
-                .averageKey("sampleRetry_" + UUID.randomUUID())
-                .averageValueSize(averageValueSize)
-                .createPersistedTo(new File(fileLocation));
+                .entries(entries)
+                .averageKey("sampleRetry" + UUID.randomUUID())
+                .averageValueSize(averageValueSize);
+    }
+
+    private ChronicleMap<String, RetryEntity> recoverMapFromInvalidHeader(Long entries, Double averageValueSize, String fileLocation, IOException iex) throws IOException {
+        ChronicleMap<String, RetryEntity> retryMap;
+        LOG.warn("Retry Map seems to be corrupt in header section. Starting recovery.", iex);
+        retryMap = createChronicleMapBuilder(entries, averageValueSize).recoverPersistedTo(new File(fileLocation), true);
+        LOG.warn("Finished recovering Retry Map.");
+        return retryMap;
+    }
+
+    private void checkIfMapHasOnlyValidEntries(ChronicleMap<String, RetryEntity> retryMapWithoutRecovery) {
+        retryMapWithoutRecovery.entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    private ChronicleMap<String, RetryEntity> recoverMapFromInvalidEntries(Long entries, Double averageValueSize, String fileLocation, RuntimeException rex) throws IOException {
+        ChronicleMap<String, RetryEntity> retryMap;
+        LOG.warn("Retry Map seems to have corrupt entries. Starting recovery.", rex);
+        retryMap = createChronicleMapBuilder(entries, averageValueSize).recoverPersistedTo(new File(fileLocation), false);
+        LOG.warn("Finished recovering Retry Map.");
+        return retryMap;
     }
 
 }
