db18a0ce5a4cda1fe2e3d2df9e43a4cf60c8b32f
Markus Kiss <markus.kiss@dm.de>
2018-03-13 15:50:14 +0100

    Added test stubs.

---
+++ src/test/java/de/dm/retrylib/RetryProcessorUnitTest.java
@@ -0,0 +1,10 @@
+package de.dm.retrylib;
+
+import org.junit.Test;
+
+public class RetryProcessorUnitTest {
+
+    @Test
+    public void processNextRetryBatch() {
+    }
+}
---
+++ src/test/java/de/dm/retrylib/RetryServiceUnitTest.java
@@ -0,0 +1,18 @@
+package de.dm.retrylib;
+
+import org.junit.Test;
+
+public class RetryServiceUnitTest {
+
+    @Test
+    public void queueForRetry() {
+    }
+
+    @Test
+    public void deleteRetryEntity() {
+    }
+
+    @Test
+    public void loadNextRetryEntities() {
+    }
+}
