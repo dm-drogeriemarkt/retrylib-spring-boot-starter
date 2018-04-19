cfae7f79826f981a1353ec7f0e3920b577a2e7e3
Patrick Hofmann <patrick.hofmann@dm.de>
2018-04-19 15:44:54 +0200

    fixed certificate path

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_be481c4eeabe_key -iv $encrypted_be481c4eeabe_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import .travis/codesigning.asc
 
     mvn deploy -P sign,build-extras --settings .travis/mvnsettings.xml
 fi
